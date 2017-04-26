c0d7eaeccce9387e11ab7a73cefe0900e10aa157
v7dobra <shherbina.i@gmail.com>
2017-04-26 14:48:02 +0300

    верстка

---
+++ index.php
@@ -0,0 +1,69 @@
+<!doctype html>
+<html lang="en">
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+	<link type="text/css" href="/css/style.css" rel="stylesheet">
+</head>
+<body>
+
+<div class="main-wrapper" id="main-page-wrapper"> <!-- 1й уровень-->
+    <div class="site-content-container">          <!-- 2й уровень-->
+        <div class="row grid-system">             <!-- 3й уровень--><!-- Повторяющийся блок-->
+	        <div class="inner-box grid-system  column-1 logo"></div>
+	        <div class="inner-box grid-system column-1 main-screen "></div>
+
+
+
+
+
+
+
+        </div>
+
+	    <div class="row grid-system">
+
+	    </div>
+
+
+
+
+    </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
+
+
+<!--
+собственные классы
+main-wrapper - главная обертка
+main-page-wrapper  обертка главной страницы
+site-content-container   Контейнер содержимого сайта
+inner-box Внутренняя коробка
+
+
+
+
+
+классы Bootstrap
+row
+grid-system
+
+
+
+-->
